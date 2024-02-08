d0efc3436dd8a57dba98891c5e2ce3eb5dba9d65
Arttanjeiro <arthursousarabelo2@gmail.com>
2024-02-07 23:24:55 -0300

    feat: Criação de modal pra card-note
    	O componet note-card já tem o modal usando ui-shadcn com
    	as estilização do figma

--- src/components/note-card.tsx
+++ src/components/note-card.tsx
@@ -1,17 +1,55 @@
 
-export function NoteCard() {
+import { Dialog, DialogClose, DialogContent, DialogOverlay, DialogPortal, DialogTrigger } from "./ui/dialog"
+import { formatDistanceToNow } from 'date-fns'
+import { ptBR } from 'date-fns/locale'
+interface NoteCardProps {
+  note: {
+    date: Date
+    content: string
+  }
+}
+
+export function NoteCard({ note }: NoteCardProps) {
   return (
-    <button className='rounded-md text-left bg-slate-800 p-5 space-y-3 overflow-hidden relative transition-all duration-300 hover:scale-105 cursor-pointer outline-none hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400  '>
-      <span className='text-sm font-medium text-slate-300'>
-        Há 2 dias
-      </span>
+    <Dialog>
+      <DialogTrigger  className='rounded-md text-left flex flex-col gap-3 bg-slate-800 p-5  overflow-hidden relative transition-all duration-300 hover:scale-105 cursor-pointer outline-none hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400  '>
+        <span className='text-sm font-medium text-slate-300'>
+          {formatDistanceToNow(note.date, { locale: ptBR, addSuffix: true })}
+        </span>
 
-      <p className='text-sm leading-6 text-slate-400'>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit sit, minima tenetur nesciunt dignissimos maiores, provident, et quo cupiditate expedita quisquam harum ducimus porro pariatur exercitationem minus reprehenderit iste. Voluptatem?
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto fuga nam nihil unde repellendus earum? Sit minus error et? Maiores molestiae ad, ullam modi excepturi error ipsam fugit blanditiis fuga?
-      </p>
-      <div className='absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none' />
+        <p className='text-sm leading-6 text-slate-400'>
+          {note.content}
+        </p>
+        <div className='absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t
+         from-black/60 to-black/0 pointer-events-none' />
+       
 
-    </button>
+        <DialogPortal>
+          <DialogOverlay className="bg-black/50 " />
+          <DialogContent className=" overflow-hidden  max-w-[640px] w-full h-[60vh]
+           bg-slate-700 rounded-md flex flex-col outline-none">
+         
+
+            <div className="flex flex-1 flex-col gap-3 p-5 ">
+              <span className='text-sm font-medium text-slate-300'>
+                {formatDistanceToNow(note.date, { locale: ptBR, addSuffix: true })}
+              </span>
+
+              <p className='text-sm leading-6 text-slate-400'>
+                {note.content}
+              </p>
+            </div>
+
+            <button type="button"
+              className="-w-full bg-slate-800 py-4 text-center outline-none text-sm
+             text-slate-300 hover:bg-slate-600 transition-all font-medium group"
+            >
+              Deseja <strong className="text-red-400 group-hover:underline"> apagar essa nota</strong>?
+            </button>
+          </DialogContent>
+
+        </DialogPortal>
+      </DialogTrigger>
+    </Dialog>
   )
 }
